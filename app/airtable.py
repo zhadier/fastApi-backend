afe9697b0347ad41065d98519c92f29b2af7f954
zhadier <zhadier39@gmail.com>
2022-08-24 18:08:42 +0500

    Add procfile for heroku

---
+++ app/airtable.py
@@ -0,0 +1,19 @@
+import os
+import requests
+from dataclasses import dataclass
+
+@dataclass()
+class Airtable:
+  base_id: str
+  api_key: str
+  table_name: str
+
+  def get_companies(self):
+    headers = {
+      "Authorization": f"Bearer {self.api_key}",
+      "Content-Type": "application/json"
+    }
+    endpoint = f'https://api.airtable.com/v0/{self.base_id}/{self.table_name}'
+    response = requests.get(endpoint, headers=headers)
+    airtable_response = response.json()
+    return airtable_response
